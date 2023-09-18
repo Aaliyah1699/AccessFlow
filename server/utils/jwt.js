64e4cbf781e7ff49cb4b5d8374c164d8712f8e69
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-09-17 19:06:02 -0500

    refactor cookies to send two

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,22 +1,40 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const createJWT = ({ payload }) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_LIFETIME,
-    });
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
     return token;
 };
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({ res, user }) => {
+const attachCookiesToResponse = ({ res, user, refreshToken }) => {
+    const accessTokenJWT = createJWT({ payload: { user } }); // keep time limit short
+    const refreshTokenJWT = createJWT({ payload: { user, refreshToken } }); // keep time limit longer
+
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    res.cookie('accessToken', accessTokenJWT, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        signed: true,
+        maxAge: 1000,
+    });
+    res.cookie('refreshToken', refreshTokenJWT, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        signed: true,
+        expires: new Date(Date.now() + oneDay),
+    });
+};
+
+const attachSingleCookieToResponse = ({ res, user }) => {
     const token = createJWT({ payload: user });
     const oneDay = 1000 * 60 * 60 * 24;
 
-    res.cookie("token", token, {
+    res.cookie('token', token, {
         httpOnly: true,
         expires: new Date(Date.now() + oneDay),
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === 'production',
         signed: true,
     });
 };
@@ -25,4 +43,5 @@ module.exports = {
     createJWT,
     isTokenValid,
     attachCookiesToResponse,
+    attachSingleCookieToResponse,
 };
